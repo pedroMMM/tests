04b3668dc122865e0a97abf6c44b4f1bf6595aee
Pedro Medeiros <do.pija@gmail.com>
2015-08-08 10:36:42 -0400

    add webapi seed

---
+++ server/server/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace server.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        string echo;
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            echo = "hit";
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "hit";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
